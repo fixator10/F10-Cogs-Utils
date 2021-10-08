536683b5c5fe7f9f9536820ea7acdc9452a3b99b
Fixator10 <github@fixator10.ru>
2021-10-08 14:52:23 +0400

    an attempt to make a python module

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="F10-Cogs-Utils",
+    description="A module with utils for Fixator10-Cogs",
+    author="Fixator10",
+    packages=["cogsutils"],
+    package_dir={"": "src"},
+)
