2075af5746c242a46d3a7d3093188cbcf4411050
Fixator10 <github@fixator10.ru>
2021-10-08 15:43:27 +0400

    Is there an award for this?

--- src/dpy_future/timestamp.py
+++ src/dpy_future/timestamp.py
@@ -3,17 +3,15 @@ from enum import Enum
 
 
 class TimestampStyle(Enum):
-    time_short = "t"
-    time_long = "T"
-    date_short = "d"
-    date_long = "D"
-    datetime_short = "f"
-    datetime_long = "F"
-    relative = "R"
+    time_short = "t"  # 16:20
+    time_long = "T"  # 16:20:30
+    date_short = "d"  # 20/04/2021
+    date_long = "D"  # 20 April 2021
+    datetime_short = "f"  # 20 April 2021 16:20 :: default
+    datetime_long = "F"  # Tuesday, 20 April 2021 16:20
+    relative = "R"  # 2 months ago
 
 
-def get_markdown_timestamp(
-    timestamp: datetime.datetime, style: TimestampStyle = TimestampStyle.datetime_short
-) -> str:
+def get_markdown_timestamp(timestamp: datetime.datetime, style: TimestampStyle = None) -> str:
     timestamp = timestamp.replace(tzinfo=datetime.timezone.utc).timestamp()
-    return f"<t:{timestamp:.0f}:{style.value}>"
+    return f"<t:{timestamp:.0f}:{style.value}>" if style else f"<t:{timestamp:.0f}>"
